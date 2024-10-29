c72a3122ed8394c212a757777dfb0108d3dde002
SanqDuonq <namsang0902s@gmail.com>
2024-10-29 20:40:25 +0700

    doing/sign-up-v1.2

---
+++ frontend/src/components/password-strength.tsx
@@ -0,0 +1,11 @@
+
+const PasswordCriteria = ({password} : {password:string}) =>{
+
+}
+
+
+export const PasswordStrength = () => {
+  return (
+    <div>password-strength</div>
+  )
+}
--- frontend/src/pages/sign-up.tsx
+++ frontend/src/pages/sign-up.tsx
@@ -2,6 +2,7 @@ import {motion} from 'framer-motion'
 import { InputComponent } from '../components/input'
 import { Lock, Mail, User } from 'lucide-react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 export const SignUpPage = () => {
     const [name,setName] = useState('')
     const [mail,setMail] = useState('')
@@ -38,7 +39,26 @@ export const SignUpPage = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
+
+                <motion.button
+                    className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white
+                    font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2
+                    focus:ring-offset-2 focus:ring-offset-gray-900 transition-all duration-200'
+                    whileHover={{scale:1.02}}
+                    whileTap={{scale:0.98}}
+                    type='submit'
+                >
+                    Sign Up
+                </motion.button>
             </form>
+        </div>
+        <div className='py-4 bg-gray-900 bg-opacity-50 text-center'>
+            <p className='text-sm text-gray-400'> 
+                Don't have account?
+                <Link to = {'/login'} className='text-green-400 hover:underline ml-1'>
+                    Login
+                </Link>
+            </p>
         </div>
     </motion.div>
   )
