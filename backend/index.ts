7d086ede68a6f87ef809b375e6b796799db64735
SanqDuonq <namsang0902s@gmail.com>
2024-11-11 23:14:21 +0700

    Fix/Deployy

--- backend/index.ts
+++ backend/index.ts
@@ -7,14 +7,9 @@ import cors from 'cors'
 dotenv.config()
 const app = express()
 
-const port = process.env.PORT || 5000
 app.use(cors({origin: 'http://localhost:5173',credentials:true}))
 app.use(express.json()); // allows us to parse incoming requests: req.body
 app.use(cookieParser()); //allows us to parse incoming cookies
 app.use('/api/auth',authRoutes)
 
-app.listen(port,() => (
-    connectDB(),
-    console.log(`Server is running port ${port}`)
-    
-))
+connectDB()
