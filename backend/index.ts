f170a133f0376ead39d76997e1c22bd71049b6c9
SanqDuonq <namsang0902s@gmail.com>
2024-11-12 08:29:52 +0700

    Deploy

--- backend/index.ts
+++ backend/index.ts
@@ -12,5 +12,6 @@ app.use(express.json()); // allows us to parse incoming requests: req.body
 app.use(cookieParser()); //allows us to parse incoming cookies
 app.use('/api/auth',authRoutes)
 connectDB()
+app.listen()
 
 export default app;
